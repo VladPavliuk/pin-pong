facf778bd6d4be1271dc974f9e03803a64891bbf
vlad <pavliuk.vlad@gmail.com>
2018-06-17 14:23:04 +0300

    add node server

---
+++ server.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+
+server.listen(process.env.PORT || 3000);
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
