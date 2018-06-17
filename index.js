d789b4d5043efd71c7d6c43922fe6fb36273d4c4
vlad <pavliuk.vlad@gmail.com>
2018-06-17 12:20:15 +0300

    delete unnecessary let keyword

--- index.js
+++ index.js
@@ -39,100 +39,90 @@ let ball = {
     };
 
 let initMap = () => {
-    ball.x = canvasElement.width / 2;
-    ball.y = canvasElement.height / 2 + 80;
-    ball.speed.x = Math.random() > .5 ? 10 : -10;
-    ball.speed.y = 0;
+        ball.x = canvasElement.width / 2;
+        ball.y = canvasElement.height / 2 + 80;
+        ball.speed.x = Math.random() > .5 ? 10 : -10;
+        ball.speed.y = 0;
 
-    user.x = 0;
-    user.y = canvasElement.height / 2;
+        user.x = 0;
+        user.y = canvasElement.height / 2;
 
-    cpu.x = canvasElement.width - cpu.width;
-    cpu.y = canvasElement.height / 2;
-};
+        cpu.x = canvasElement.width - cpu.width;
+        cpu.y = canvasElement.height / 2;
+    },
+    clearCanvas = () => {
+        canvasContext.fillStyle = '#000';
+        canvasContext.fillRect(0, 0, canvasElement.width, canvasElement.height);
+    },
+    drawScore = (score, font = 30) => {
+        canvasContext.font = font + "px Arial";
+        canvasContext.fillStyle = '#fff';
+        canvasContext.fillText("Your score: " + score, canvasElement.width / 2 - 90, 50);
+    },
+    userLoseEvent = () => {
+        isGameOver = true;
+    },
+    userWinEvent = () => {
+        user.score++;
+        drawScore(user.score);
+        initMap();
+    },
+    moveUser = (direction) => {
+        user.y += direction;
 
-let clearCanvas = () => {
-    canvasContext.fillStyle = '#000';
-    canvasContext.fillRect(0, 0, canvasElement.width, canvasElement.height);
-};
+        if (user.y < 0) {
+            user.y = 0;
+        } else if (user.y + user.height > canvasElement.height) {
+            user.y = canvasElement.height - user.height;
+        }
+    },
+    drawUser = () => {
+        canvasContext.fillStyle = user.color;
+        canvasContext.fillRect(user.x, user.y, user.width, user.height);
+    },
+    moveBall = () => {
+        ball.x += ball.speed.x;
+        ball.y += ball.speed.y;
 
-let drawScore = (score, font = 30) => {
-    canvasContext.font = font + "px Arial";
-    canvasContext.fillStyle = '#fff';
-    canvasContext.fillText("Your score: " + score, canvasElement.width / 2 - 90, 50);
-};
+        if (ball.y + ball.height >= canvasElement.height || ball.y <= 0) {
+            ball.speed.y = -ball.speed.y;
+        }
 
-let userLoseEvent = () => {
-    isGameOver = true;
-};
+        if (ball.x - ball.speed.x >= 0 && ball.x - ball.speed.x <= user.x + user.width
+            && ball.y > user.y - ball.height && ball.y < user.y + user.height) {
+            ball.x = user.x + user.width + 1;
+            ball.speed.x = -ball.speed.x;
+            ball.speed.y = -1 * ball.speed.y + Math.random() * (5 - 1) + 1;
+        }
 
-let userWinEvent = () => {
-    user.score++;
-    drawScore(user.score);
-    initMap();
-};
+        if (ball.x + ball.speed.x <= canvasElement.width && ball.x + ball.speed.x >= cpu.x - cpu.width
+            && ball.y > cpu.y - ball.height && ball.y < cpu.y + cpu.height) {
+            ball.x = cpu.x - cpu.width - 1;
+            ball.speed.x = -ball.speed.x;
+            ball.speed.y = -1 * ball.speed.y + Math.random() * (5 - 1) + 1;
+        }
 
-let moveUser = (direction) => {
-    user.y += direction;
-
-    if (user.y < 0) {
-        user.y = 0;
-    } else if (user.y + user.height > canvasElement.height) {
-        user.y = canvasElement.height - user.height;
-    }
-};
-
-let drawUser = () => {
-    canvasContext.fillStyle = user.color;
-    canvasContext.fillRect(user.x, user.y, user.width, user.height);
-};
-
-let moveBall = () => {
-    ball.x += ball.speed.x;
-    ball.y += ball.speed.y;
-
-    if (ball.y + ball.height >= canvasElement.height || ball.y <= 0) {
-        ball.speed.y = -ball.speed.y;
-    }
-
-    if (ball.x - ball.speed.x >= 0 && ball.x - ball.speed.x <= user.x + user.width
-        && ball.y > user.y - ball.height && ball.y < user.y + user.height) {
-        ball.x = user.x + user.width + 1;
-        ball.speed.x = -ball.speed.x;
-        ball.speed.y = -1 * ball.speed.y + Math.random() * (5 - 1) + 1;
-    }
-
-    if (ball.x + ball.speed.x <= canvasElement.width && ball.x + ball.speed.x >= cpu.x - cpu.width
-        && ball.y > cpu.y - ball.height && ball.y < cpu.y + cpu.height) {
-        ball.x = cpu.x - cpu.width - 1;
-        ball.speed.x = -ball.speed.x;
-        ball.speed.y = -1 * ball.speed.y + Math.random() * (5 - 1) + 1;
-    }
-
-    if (ball.x < 0) {
-        userLoseEvent();
-    } else if (ball.x + ball.width> canvasElement.width) {
-        userWinEvent();
-    }
-};
-
-let drawBall = () => {
-    canvasContext.fillStyle = ball.color;
-    canvasContext.fillRect(ball.x, ball.y, ball.width, ball.height);
-};
-
-let moveCpu = () => {
-    if (ball.y > cpu.y + cpu.height / 2 && cpu.y + cpu.height < canvasElement.height) {
-        cpu.y += cpu.speed.y;
-    } else if (ball.y < cpu.y) {
-        cpu.y -= cpu.speed.y;
-    }
-};
-
-let drawCpu = () => {
-    canvasContext.fillStyle = cpu.color;
-    canvasContext.fillRect(cpu.x, cpu.y, cpu.width, cpu.height);
-};
+        if (ball.x < 0) {
+            userLoseEvent();
+        } else if (ball.x + ball.width > canvasElement.width) {
+            userWinEvent();
+        }
+    },
+    drawBall = () => {
+        canvasContext.fillStyle = ball.color;
+        canvasContext.fillRect(ball.x, ball.y, ball.width, ball.height);
+    },
+    moveCpu = () => {
+        if (ball.y > cpu.y + cpu.height / 2 && cpu.y + cpu.height < canvasElement.height) {
+            cpu.y += cpu.speed.y;
+        } else if (ball.y < cpu.y) {
+            cpu.y -= cpu.speed.y;
+        }
+    },
+    drawCpu = () => {
+        canvasContext.fillStyle = cpu.color;
+        canvasContext.fillRect(cpu.x, cpu.y, cpu.width, cpu.height);
+    };
 
 document.onkeydown = (event) => {
     const key = event.key;
@@ -145,7 +135,7 @@ document.onkeydown = (event) => {
 };
 
 let gameIteration = () => {
-    if(!isGameOver) {
+    if (!isGameOver) {
         clearCanvas();
         drawScore(user.score);
         moveBall();
